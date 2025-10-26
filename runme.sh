5a5d14c6b10124a872bb09bffada498fb8e76232
unnamed <ni.kuchmenko@gmail.com>
2025-10-27 02:16:45 +0300

    Enhance runme.sh to install and configure Neovim
    Added commands to copy binaries and update PATH.

--- runme.sh
+++ runme.sh
@@ -1,2 +1,14 @@
 # install packet manager
 git clone https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/lazy/lazy.nvim
+nvim --headless "+Lazy! sync" +qa
+
+# Копируем бинарники в системные директории
+sudo cp -r nvim-linux-x86_64/bin/* /usr/local/bin/
+sudo cp -r nvim-linux-x86_64/lib/* /usr/local/lib/
+sudo cp -r nvim-linux-x86_64/share/* /usr/local/share/
+
+rm -rf nvim-linux-x86_64
+
+
+printf 'export PATH="/usr/local/bin:$PATH"\n' > ~/.bashrc
+source ~/.bashrc
